80475fa89ef6f15443ce407b78f46d80c5479b02
mkotte <74936870+mkotte@users.noreply.github.com>
2021-06-22 20:52:03 -0400

    completed service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,4 @@
+// framed from week 18 activity 23
 const FILES_TO_CACHE = [
     '/',
     '/index.html',
@@ -11,5 +12,52 @@ const FILES_TO_CACHE = [
 const PRECACHE = 'budget-tracker-cache-v1';
 const RUNTIME = 'runtime';
 
-// installing caches
-self.addEventListener()
+// Installing caches
+self.addEventListener("install", function(event) {
+    event.waitUntil(
+        caches
+            .open(PRECACHE)
+            .then((cache) => cache.addAll(FILES_TO_CACHE))
+            .then(self.skipWaiting())
+    )
+});
+
+// "activate handler" clearing up old caches
+self.addEventListener('activate', (event => {
+    const currentCaches = [PRECACHE, RUNTIME];
+    event.waitUntil(
+        caches
+            .keys()
+            .then((cacheNames) => {
+                return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+            })
+            .then((cachesToDelete) => {
+                return Promise.all(
+                    cachesToDelete.mao((cacheToDelete) => {
+                        return caches.delete(cacheToDelete);
+                    })
+                );
+            })
+            .then(() => self.clients.claim())        
+    );
+}));
+
+// "fetch handler" 
+self.addEventListener('fetch', (event) => {
+    if (event.request.url.startsWith(self.location.origin)) {
+        event.respondWith(
+            caches.match(event.request).then((cachedResponse) => {
+                if (cachedResponse) {
+                    return cachedResponse
+                }
+                return caches.open(RUNTIME).then((cache) => {
+                    return fetch(event.request).then((response) => {
+                        return cache.put(event.request, response.clone()).then(() => {
+                            return response;
+                        });
+                    });
+                });
+            })
+        );
+    }
+});
